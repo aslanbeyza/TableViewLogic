941e42ef9e69764dafd7c85e9a5d05f9d1b628ff
aslanbeyza <aslanbeyza3413@gmail.com>
2025-02-23 19:21:22 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Beyza Aslan on 23.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
